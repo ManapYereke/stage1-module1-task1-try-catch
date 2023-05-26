4205aa70953fe3f2a75b2e1678f64d99d136cf1a
ManapYereke <manap.ereke@gmail.com>
2023-05-26 15:22:40 +0600

    yereke

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -5,15 +5,15 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
+ * Complete the code, parse integers, calculate the sum of numbers in the WORDS,
+ * join strings with
  * space delimiter
  */
 public class ParseIntegers {
 
-    private static final List<String> WORDS =
-            Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                            .split(" "));
+    private static final List<String> WORDS = Arrays.asList(
+            "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                    .split(" "));
 
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
@@ -21,11 +21,14 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                justWords += " " + next;
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
 }
-
